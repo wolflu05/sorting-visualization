9b900206c5b7e56a3b59739d1a95b07410f3f13a
wolflu05 <76838159+wolflu05@users.noreply.github.com>
2023-07-24 10:37:13 +0200

    Removed unnecessary dependencies

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 import '@fontsource/roboto';
 
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 import './index.css';
 
@@ -14,8 +13,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
